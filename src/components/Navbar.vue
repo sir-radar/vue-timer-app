009c9802dbd55a15db4f99f517b5aff88052246d
Sir_radar <samson.nwokike@demzanalytics.com>
2019-03-14 03:29:26 +0100

    added navigation drawer to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,28 @@
 <template>
     <nav>
         <v-toolbar app dark class="secondary">
-            <v-toolbar-side-icon/>
+            <v-toolbar-side-icon @click="isDrawerOpen = !isDrawerOpen"/>
             <v-toolbar-title class="headline text-uppercase">
-                <span>Vue</span>
-                <span>Timer</span>
+                <span class="font-weight-black primary--text">Vue</span>
+                <span class="font-weight-light">Timer</span>
             </v-toolbar-title>
         </v-toolbar>
+
+        <v-navigation-drawer app dark v-model="isDrawerOpen">
+            <v-toolbar class="secondary">
+                <v-toolbar-title class="title">Laps</v-toolbar-title>
+            </v-toolbar>
+        </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
 export default {
-
+ data(){
+     return {
+        isDrawerOpen: false 
+     }
+ }
 }
 </script>
 

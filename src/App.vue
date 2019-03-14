8d41d8451a56dc122e8949cc30ef0d624de45e00
Sir_radar <samson.nwokike@demzanalytics.com>
2019-03-14 03:42:59 +0100

    setup timer component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,30 @@
   <v-app>
     <Navbar/>
     <v-content>
-     
+     <Timer :timer="formattedTime" :state="timerState"/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Timer from './components/Timer';
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Timer
   },
   data () {
     return {
-      //
+      timerState: 'stopped',
+      currentTimer: 0,
+      formattedTime: "00:00:00",
+      ticker: null,
+      taps: [],
+      lastestLap: "",
+      snackbar: false
     }
   }
 }
---
+++ src/components/Timer.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container fluid>
+        <v-layout>
+            <v-flex xs12 pa=3>
+                <v-card>
+                    <v-card-title class="secondary white--text font-weight-light display-2
+                    text-uppercase justify-center">
+                        {{ timer }}
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props:['timer', 'state']
+}
+</script>
+
+<style>
+
+</style>

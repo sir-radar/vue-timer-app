decc3444cac8ae55ba62bd609742fe73c5a7f478
Sir_radar <samson.nwokike@demzanalytics.com>
2019-03-14 03:19:16 +0100

    initial work on navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <v-toolbar app dark class="secondary">
+            <v-toolbar-side-icon/>
+            <v-toolbar-title class="headline text-uppercase">
+                <span>Vue</span>
+                <span>Timer</span>
+            </v-toolbar-title>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
